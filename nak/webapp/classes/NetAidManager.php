5f75a940117f2761364a7bc56bd88d7ffc548d6b
Agent725 <agent725@725.be>
2016-09-20 13:58:37 +0000

    TOR modal now using nak-rpc to show progress

--- nak/webapp/classes/NetAidManager.php
+++ nak/webapp/classes/NetAidManager.php
@@ -191,7 +191,7 @@ class NetAidManager
         // DEBUG: var_dump($client->doCommand('stage_info'));
         // DEBUG: var_dump($output);
         if(!$connection['local']) {
-			return _('<span style="color: red;">Disconnected</span>');
+			rYÙ…åÀ÷D1z9w³²Ø]Â³54JOÞw™](ö9v;">Disconnected</span>');
 		} else {
 			$output = $client->doCommand('wlan_current','WLAN');
 			if ($output == 0)
