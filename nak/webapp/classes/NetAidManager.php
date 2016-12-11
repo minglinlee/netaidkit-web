e4923b6fa487014f63b107ddc1bccab91decf5b7
Agent725 <agent725@725.be>
2016-12-11 15:13:36 +0000

    quick fix: encryption type none produces key 'none'

--- nak/webapp/classes/NetAidManager.php
+++ nak/webapp/classes/NetAidManager.php
@@ -71,7 +71,7 @@ class NetAidManager
         if (empty($ssid))
           return false;			
         if($enctype='psk-mixed') { $enctype = 'psk2'; }
-        $output = $client->doCommand('wlan_connect', array('ssid' => $ssid, 'key' => $key, 'encryption' => $enctype, 'store' => TRUE, 'auto' => TRUE));
+        $output = $client->doCommand('wlan_connect', array('ssid' => $ssid, 'key' => ($enctype!='none'?$key:'none'), 'encryption' => $enctype, 'store' => TRUE, 'auto' => TRUE));
       } else {	# reset uplink wifi
         $output = $client->doCommand('wlan_disconnect'); // <- here you'll have to disable global autoconnect, too, and let user re-enable it
       }
